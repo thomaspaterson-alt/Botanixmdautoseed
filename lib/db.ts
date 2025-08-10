43e9410e86046f28c8cfb0f76841ff1fdb32ef90
thomaspaterson-alt <thomas.paterson@gmail.com>
2025-08-09 18:55:13 -0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from "@prisma/client"
+// lib/db.ts
+import { PrismaClient } from "@prisma/client";
 
-if (!process.env.DATABASE_URL && process.env.POSTGRES_PRISMA_URL) {
-  process.env.DATABASE_URL = process.env.POSTGRES_PRISMA_URL
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-declare global { var prisma: PrismaClient | undefined }
-export const prisma = global.prisma ?? new PrismaClient()
-if (process.env.NODE_ENV !== "production") global.prisma = prisma
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
