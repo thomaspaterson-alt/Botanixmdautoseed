9df06a0fcf3d2481099ba5a538c3e533f5124c47
thomaspaterson-alt <thomas.paterson@gmail.com>
2025-08-09 18:58:41 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+// next.config.js
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: true },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }
+module.exports = {
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+  webpack: (config) => {
+    // If you *ever* want to go back to "@/..." paths, uncomment:
+    // config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
-export default nextConfig;
